03f29b2e401d58627133af6156a59f5d13d26e96
Nilesh G <nilesh.gavaskar@spirent.com>
2013-07-09 00:58:47 +0000

    Web.js

--- web.js
+++ web.js
@@ -6,8 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fileBuf = new Buffer(fs.readFileSync("index.html"));
-    fileBuf.toString();
-    response.send(fileBuf.write());
+    response.send(fileBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
